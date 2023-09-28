808c3e5bb0489d31a1a747e69af24d8e443f813e
helloswx <helloswx@126.com>
2023-09-28 14:35:43 +0800

    Update bashdb.go

--- mongo/bashdb.go
+++ mongo/bashdb.go
@@ -2,26 +2,26 @@ package mongo
 
 // Databse 1, store the basic transaction metadata
 type Transac struct {
-	// Transaction 
+	// Transaction
 	Tx_BlockHash string
-	Tx_BlockNum uint64 
-	Tx_FromAddr string
-	Tx_Gas string
-	Tx_GasPrice string
-	Tx_Hash string 
-	Tx_Input string 
-	Tx_Nonce uint64
-	Tx_ToAddr string
-	Tx_Index string
-	Tx_Value string
+	Tx_BlockNum  uint64
+	Tx_FromAddr  string
+	Tx_Gas       string
+	Tx_GasPrice  string
+	Tx_Hash      string
+	Tx_Input     string
+	Tx_Nonce     uint64
+	Tx_ToAddr    string
+	Tx_Index     string
+	Tx_Value     string
 
 	Tx_Trace string
 
-	Re_contractAddress string
+	Re_contractAddress   string
 	Re_CumulativeGasUsed string
-	Re_GasUsed string
-	Re_Status  string
-	Re_FailReason string
+	Re_GasUsed           string
+	Re_Status            string
+	Re_FailReason        string
 }
 
 // 默认为50， 这里改成1仅为了测试
