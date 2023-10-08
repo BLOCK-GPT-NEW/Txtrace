04f62cedcb786327e866f46433e19044e1bd5e38
hisky <c919670942@gmail.com>
2023-10-08 14:16:10 +0800

    Update bashdb.go

--- mongo/bashdb.go
+++ mongo/bashdb.go
@@ -9,19 +9,22 @@ type Transac struct {
 	Tx_Gas       string
 	Tx_GasPrice  string
 	Tx_Hash      string
-	Tx_Input     string
+	Tx_Input     []byte
 	Tx_Nonce     uint64
 	Tx_ToAddr    string
 	Tx_Index     string
 	Tx_Value     string
 
-	Tx_Trace string
+	// Tx_Trace string
 
 	Re_contractAddress   string
 	Re_CumulativeGasUsed string
 	Re_GasUsed           string
 	Re_Status            string
 	Re_FailReason        string
+}
+type Trace struct {
+	Tx_Trace string
 }
 
 // 默认为50， 这里改成1仅为了测试
